01af1e8057e99bc576f9c38875323c0844704ffc
Nicholas Foreman <nicholas.m.foreman@gmail.com>
2020-04-28 01:01:23 -0500

    Edit Javascript Code.

--- Develop/script.js
+++ Develop/script.js
@@ -8,10 +8,12 @@ function writePassword() {
 }
 
 function generatePassword() {
-  var lowerCase = "abcdefghijklmnopqrstuvwxyz";
-  var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  var numeric = "0123456789";
-  var special = "~!@#$%^&*()-_=+<>?";
+  // Establishing the different variables.
+  var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+  var numericChar = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+  var specialChar = ["~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+", "<", ">", "?"];
+  var allCharTypes = [lowerCase, upperCase, numericChar, specialChar];
   var passwordLength = prompt("How long would you like your password to be?");
 
   if (passwordLength < 8) {
@@ -40,14 +42,10 @@ function generatePassword() {
   // Validate Input
   console.log(passwordCriteria);
 
-  // Ensures that at least one of the criteria is selected.
-  if (answer === false) {
-    alert("You must select at least one choice for your character type.");
+  for (var i = 0; i < passwordLength; i++) {
+    var num = Math.floor(Math.random() * Math.floor(128))
   }
-  else {
-    passwordText.value = password;
-  };
-}
-
+  passwordText.value = password;
+};
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
