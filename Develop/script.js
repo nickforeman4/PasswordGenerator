b4a30f45aa2950c85e1c226162e88bd8ef4b5f53
Nicholas Foreman <nicholas.m.foreman@gmail.com>
2020-04-30 19:48:21 -0500

    Editing script code.

--- Develop/script.js
+++ Develop/script.js
@@ -5,47 +5,62 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
+      passwordText.value = password;
+
 }
 
 function generatePassword() {
-  // Establishing the different variables.
-  var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-  var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-  var numericChar = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-  var specialChar = ["~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+", "<", ">", "?"];
-  var allCharTypes = [lowerCase, upperCase, numericChar, specialChar];
-  var passwordLength = prompt("How long would you like your password to be?");
+// Variables for generating a password
+  var lowerCase = "abcdefghijklmnopqrstuvwxyz"
+  var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  var numericChar = "1234567890"
+  var specialChar = "!@#$%^&*()_"
+  var allCharTypes = "";
+  var passwordLength = parseInt(prompt("How long would you like your password to be?"));
 
-  if (passwordLength < 8) {
+  if (passwordLength < 8 || passwordLength > 128) {
     alert("Your password must be between 8 and 128 characters. Please enter a number within that range.");
-    prompt("How long would you like your password to be?");
   }
-  else if (passwordLength > 128) {
-    alert("Your password must be between 8 and 128 characters. Please enter a number within that range.");
-    prompt("How long would you like your password to be?");
+  else {
+
+    var lowerCaseChoice = confirm("Would you like your password to contain lower case characters?");
+
+    var upperCaseChoice = confirm("Would you like your password to contain upper case characters?");
+
+    var numericCharChoice = confirm("Would you like your password to contain numeric characters?");
+
+    var specialCharChoice = confirm("Would you like your password to contain special characters?");
+
+    var passwordCriteria = [lowerCaseChoice, upperCaseChoice, numericCharChoice, specialCharChoice];
+
+    if (!lowerCaseChoice && !upperCaseChoice && !numericCharChoice && !specialCharChoice) {
+      alert("You must select at least one choice for your character type");
+    }
+    else {
+      if (lowerCaseChoice) {
+      allCharTypes+=lowerCase;
+      }
+      if (upperCaseChoice) {
+        allCharTypes+=upperCase
+      }
+      if (numericCharChoice) {
+        allCharTypes+=numericChar
+      }
+      if (specialCharChoice) {
+        allCharTypes = allCharTypes+specialChar
+      }
+      // Validate Input
+      console.log(allCharTypes);
+      var gpassword = ""
+      for (var i = 0; i < passwordLength; i++) {
+        var character = allCharTypes[Math.floor(Math.random() * allCharTypes.length)]
+        console.log(character)
+        gpassword += character
+      }
+      console.log("Password", gpassword)
+      return gpassword
+    }
   }
-  
-  var lowerCaseChoice = confirm("Would you like your password to contain lower case characters?");
-
-  var upperCaseChoice = confirm("Would you like your password to contain upper case characters?");
-
-  var numericCharChoice = confirm("Would you like your password to contain numeric characters?");
-
-  var specialCharChoice = confirm("Would you like your password to contain special characters?");
-  
-  var passwordCriteria = [lowerCaseChoice, upperCaseChoice, numericCharChoice, specialCharChoice];
-
-  if (!lowerCaseChoice && !upperCaseChoice && !numericCharChoice && !specialCharChoice) {
-    alert("You must select at least one choice for your character type");
-  }
-    
-  // Validate Input
-  console.log(passwordCriteria);
-
-  for (var i = 0; i < passwordLength; i++) {
-    var num = Math.floor(Math.random() * Math.floor(128))
-  }
-  passwordText.value = password;
 };
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
